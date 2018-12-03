aeac616c684d979a77812171a8a88cc04f3e0dc3
alexander3435 <45250031+alexander3435@users.noreply.github.com>
2018-12-03 23:59:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,29 +83,6 @@ client.on('message', message => {
 
 
 
-
-
-client.on("message", message => {
-  if(message.content.startsWith("/verify")) { // الامر والبريفكس
-    let num = Math.floor((Math.random() * 4783) + 10);
-
-    message.channel.send(`يرجاء كتابة الرقم التالي: **${num}**`).then(m => {
-      message.channel.awaitMessages(res => res.content == `${num}`, {
-        max: 1,
-        time: 60000,
-        errors: ['time'],
-      }).then(collected => {
-        message.delete();
-        m.delete();
-        message.member.addRole(message.guild.roles.find(c => c.name == "●<[Members]>●")); // اسم الرتبة
-      }).catch(() => {
-        m.edit(`You took to long to type the number.\nRe-type the command again if you want to verify yourself.`).then(m2 => m.delete(15000));
-      });
-    });
-  }
-});
-
-
 	client.on('message', async message => {
   if(message.content.startsWith(prefix + "طلب")) {
     await message.channel.send("**ماذا تريد:small_orange_diamond:**").then(e => {
@@ -785,59 +762,86 @@ client.on('guildMemberAdd', member => {
 
 
 
-client.on('message', message => {
-  if (message.content.startsWith('هلا بيك')) {
-    const voiceChannel = message.member.voiceChannel;
-    voiceChannel.join()
-      .then(connnection => {
-        const stream = ytdl("https://ytcropper.com/cropped/rB5c057f5ca4911", { filter: 'audioonly' });
-        const dispatcher = connnection.playStream(stream);
-                dispatcher.on('end', () => voiceChannel.leave());
-
-      });///////////MaL Team 
-  }
-})//////////////////////////Mal Team
 
 
 
 
+client.on('guildMemberAdd', member => {
+     const welcomer =  member.guild.channels.find('name', 'the-haven');
+    if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var m = member.user;
+        let yumz = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(m.avatarURL)
+        .setAuthor(m.username,m.avatarURL)
+        .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)            
+      
+         .setFooter(`${m.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:yumz});          
+         
+    
 
-const bannedwords = [
-    " كسمك",
-    "كسمين امك",
-    "الشتيمه",
-    "الشتيمه",
-    "الشتيمه",
-    "الشتيمه"
 
-  ];
 
-client.on('message',  message => {
-  if(bannedwords.some(word => message.content.includes(word))) {
-    message.delete()
-    message.reply(" احترم نفسك , يمنج الشتايم تمامنا هنا  ").then(msg => {msg.delete(5000)});;
-  };
+const w = ['./img/w1.png'];
+
+         let Image = Canvas.Image,
+            canvas = new Canvas(400, 200),
+            ctx = canvas.getContext('2d');
+        fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
+            if (err) return console.log(err);
+            let BG = Canvas.Image;
+            let ground = new Image;
+            ground.src = Background;
+            ctx.drawImage(ground, 0, 0, 400, 200);
+             
+          
+
+                let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(100) + ".png" : member.user.displayAvatarURL;
+                jimp.read(url, (err, ava) => {
+                    if (err) return console.log(err);
+                    ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
+                        if (err) return console.log(err);
+                        
+                        ctx.font = "bold 12px Arial";
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`welcome to ${member.guild.name}`, 300, 130);
+                        
+                        ctx.font = "bold 12px Arial";
+                        ctx.fontSize = '20px';
+                        ctx.fillStyle = "#f1f1f1";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 200, 150);
+ 
+                let Avatar = Canvas.Image;
+                              let ava = new Avatar;
+                              ava.src = buf;
+                              ctx.beginPath();
+                              ctx.arc(77, 101, 62, 0, Math.PI*2);
+                              ctx.stroke();
+                                 ctx.clip();
+                                 ctx.drawImage(ava, 13, 38, 128, 126);  
+                          
+                
+                             
+welcomer.sendFile(canvas.toBuffer())
+
+
+
+      
+      
+                    }  )  
+      
+                    
+
+})
+      });                    
+ }
 });
-
-
-
-
-
-
-client.on('message', message => {
-    if (message.content === 'بوم') {
-          let count = 1;
-          let ecount = 1;
-          for(let x = 1; x < 99999; x++) {
-            message.channel.send(`ٍٍٍ`)
-              .then(m => {
-                count++;
-              })
-              
-            }
-          }
-    });
-
 
 
 
